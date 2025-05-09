7f5f24ce3e6f3af313d76fea8df7251243f720c4
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-09 21:53:40 +0430

    l

--- Tmrin-js/Tamrin.js
+++ Tmrin-js/Tamrin.js
@@ -1,16 +1,16 @@
 const input = document.getElementById("input");
 const btnSave = document.getElementById("btnSave");
-const Text = [];
+const todo = [];
 let Edettixt = -1;
 btnSave.addEventListener("click", () => {
   const value = input.value.trim();
   if (value === "") return;
   if (Edettixt === -1) {
-    text.push.(value);
-  }
-  else{
+    todo.push(value);
+  } else {
     text[Edettixt] = value;
     Edettixt = -1;
-  };
+  }
   input.value = "";
 });
+function git() {}
