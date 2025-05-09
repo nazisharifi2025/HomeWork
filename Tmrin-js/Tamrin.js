aabb3a77524054dc0389f105f750b738121cf6c5
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-09 21:48:03 +0430

    l

--- Tmrin-js/Tamrin.js
+++ Tmrin-js/Tamrin.js
@@ -6,5 +6,11 @@ btnSave.addEventListener("click", () => {
   const value = input.value.trim();
   if (value === "") return;
   if (Edettixt === -1) {
+    text.push.(value);
   }
+  else{
+    text[Edettixt] = value;
+    Edettixt = -1;
+  };
+  input.value = "";
 });
