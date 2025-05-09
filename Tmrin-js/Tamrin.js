3f72b7e0ae05a1493050398c14a4c23838b85a93
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-09 21:31:36 +0430

    l

--- Tmrin-js/Tamrin.js
+++ Tmrin-js/Tamrin.js
@@ -1,2 +1,6 @@
 const input = document.getElementById("input");
 const btnSave = document.getElementById("btnSave");
+const Text = [];
+btnSave.addEventListener("click", () => {
+  const value = input.value.trim();
+});
