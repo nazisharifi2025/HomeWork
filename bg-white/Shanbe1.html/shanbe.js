ab1252a733f2109e79640f53f5b8c18ec6414f7b
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-11 21:50:40 +0430

    l

--- bg-white/Shanbe1.html/shanbe.js
+++ bg-white/Shanbe1.html/shanbe.js
@@ -1,13 +1,13 @@
 // const observer = new IntersectionObserver(
 //   (entres) => {
 //     entres.forEach((entry) => {
-//       if (entry.isIntersecting) {
-//         entry.target.classList.add("animate-fade-up-show");
-//       } else {
-//         entry.target.classList.remove("animate-fade-up-show");
-//       }
-//     });
-//   },
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate-fade-up-show");
+      } else {
+        entry.target.classList.remove("animate-fade-up-show");
+      }
+    });
+  },
   {
     threshold: 0.3,
   }
