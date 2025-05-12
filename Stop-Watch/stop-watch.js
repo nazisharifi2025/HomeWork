316ca3be3234973116533353694e89c4e7f63ea0
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-12 21:05:03 +0430

    l

---
+++ Stop-Watch/stop-watch.js
@@ -0,0 +1,34 @@
+// // const min = document.getElementById("min");
+// // const sec = document.getElementById("sec");
+// // const milis = document.getElementById("milis");
+// const play = document.getElementById("play");
+// const stop = document.getElementById("stop");
+// const button = document.getElementById("button");
+// let intervalid;
+// let startTime;
+// let currentTime = new Date();
+// let minet = currentTime.getMinutes();
+// let secound = currentTime.getSeconds();
+// let milisecound = currentTime.getMilliseconds();
+// play.addEventListener("click", () => {
+//   play.style.display = "none";
+//   startTime = Date.now();
+//   stop.style.display = "block";
+//   Watch();
+// });
+// function Watch() {
+//   setInterval(() => {
+//     let currentTime = new Date();
+//     let minet = currentTime.getMinutes();
+//     let secound = currentTime.getSeconds();
+//     let milisecound = currentTime.getMilliseconds();
+//     document.getElementById("min").innerHTML = minet;
+//     document.getElementById("sec").innerHTML = secound;
+//     document.getElementById("milis").innerHTML = milisecound;
+//   }, 10);
+// }
+// stop.addEventListener("click", () => {
+//   clearInterval(intervalid);
+//   stop.style.display = "none";
+//   play.style.display = "block";
+// });
